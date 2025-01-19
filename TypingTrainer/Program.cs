8e87057ebcadb2f0eee19b0e371f8816e523dcc6
Hossein Hajizadeh <hhsaffar@gmail.com>
2025-01-19 21:17:27 +0330

    Program entry point

---
+++ TypingTrainer/Program.cs
@@ -0,0 +1,13 @@
+﻿using TypingTrainer;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ITrainer trainer = new RandomTrainer();
+
+        ITrainingDriver trainingDriver = new TrainingDriver(trainer);
+
+        trainingDriver.Run();
+    }
+}
